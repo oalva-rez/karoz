2684f9ddd0d283780608fc751d3437e089817a08
Ozkar Alvarez <ozkaralvarez98@hotmail.com>
2022-07-27 15:19:02 -0700

    App state init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React, { useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Header from "./components/Header/Header";
+import CurrentBoard from "./components/CurrentBoard/CurrentBoard";
 
 export default function App() {
-  return <></>;
+  const [darkTheme, setDarkTheme] = useState(false);
+  const [currentTask, setCurrentTask] = useState("");
+  const [currentBoard, setCurrentBoard] = useState({
+    title: "",
+    id: "",
+  });
+  const [boards, setBoards] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <CurrentBoard />
+    </>
+  );
 }
